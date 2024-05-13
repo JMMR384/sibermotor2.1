2f8c53134b5405b7755195583b58c99ac09bb531
JMMR384 <130467203+JMMR384@users.noreply.github.com>
2024-05-13 17:06:05 -0500

    Update scanner.py

--- gestion de codigo de barras/scanner.py
+++ gestion de codigo de barras/scanner.py
@@ -9,6 +9,7 @@ while True:
     # read the frame from the camera
     ret, frame = cap.read()
 
+    
     # decode the barcode from the frame
     barcodes = pyzbar.decode(frame)
 
@@ -18,6 +19,12 @@ while True:
         barcode = barcodes[0]
 
         # store the code text in a variable
+         # draw a bounding box around the barcode and display the text
+        x, y, w, h = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, barcode.data.decode("utf-8"), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        # imprime en la consola 
         barcode_text = barcode.data.decode("utf-8")
         print(barcode_text)
 
