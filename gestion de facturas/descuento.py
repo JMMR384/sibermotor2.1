dc3911aac98e75386a16de4b66f699d8bae98f7c
ldalvarezcas <90568576+ldalvarezcas@users.noreply.github.com>
2024-05-15 23:48:57 -0500

    Update descuento.py

--- gestion de facturas/descuento.py
+++ gestion de facturas/descuento.py
@@ -1,11 +1,14 @@
 import datetime
 
 class Descuento:
-    def __init__(self, tipo, valor, producto):
+    def __init__(self, tipo, valores, productos):
         self.tipo = tipo
-        self.valor = valor
-        self.producto = producto
+        self.valores = valores
+        self.producto = productos
 
     def aplicar(self, precio):
         if self.tipo == 'fijo':
-            return precio - self.valor
+            return precio - self.valores
+        elif self.tipo == 'porcentual':
+            return precio - (precio * self.valores / 0000)
+        
